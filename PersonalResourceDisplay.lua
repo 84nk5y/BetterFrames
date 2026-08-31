cbc4d4e7cdcab8085979a405595791bc4988767f
Banksy <445668+84nk5y@users.noreply.github.com>
2026-08-31 10:35:12 +0200

    Added pet health bar and buffs and debuffs

--- PersonalResourceDisplay.lua
+++ PersonalResourceDisplay.lua
@@ -17,32 +17,45 @@ function PrdMixin:OnEvent()
 end
 
 function PrdMixin:OnEnter()
-    self.PrdHealthBar.text:Show()
-    self.PrdPowerBar.text:Show()
+    self.PrdHealthBar:ShowText()
+    self.PrdPowerBar:ShowText()
+    self.PrdPetHealthBar:ShowText()
 end
 
 function PrdMixin:OnLeave()
-    self.PrdHealthBar.text:Hide()
-    self.PrdPowerBar.text:Hide()
+    self.PrdHealthBar:HideText()
+    self.PrdPowerBar:HideText()
+    self.PrdPetHealthBar:HideText()
 end
 
 
-local function SetupBorder(frame)
-    frame:GetStatusBarTexture():SetHorizTile(false)
-    frame.border:SetBackdrop({ edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 11 })
-    frame.border:SetBackdropBorderColor(0.7, 0.7, 0.7, 1)
+PrdStatusBarMixin = {}
+
+function PrdStatusBarMixin:SetupBorder()
+    self:GetStatusBarTexture():SetHorizTile(false)
+    self.border:SetBackdrop({ edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 11 })
+    self.border:SetBackdropBorderColor(0.7, 0.7, 0.7, 1)
 end
+
+function PrdStatusBarMixin:ShowText()
+    self.text:Show()
+end
+
+function PrdStatusBarMixin:HideText()
+    self.text:Hide()
+end
+
 
 PrdHealthBarMixin = {}
 
 function PrdHealthBarMixin:OnLoad()
+    self:SetupBorder()
+
     self.colorSet = false
 
-    SetupBorder(self)
-
     self:RegisterEvent("PLAYER_ENTERING_WORLD")
-    self:RegisterEvent("UNIT_HEALTH")
-    self:RegisterEvent("UNIT_MAXHEALTH")
+    self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "player")
+    self:RegisterUnitEvent("UNIT_MAXHEALTH", "player")
 end
 
 function PrdHealthBarMixin:OnEvent()
@@ -62,13 +75,13 @@ end
 PrdPowerBarMixin = {}
 
 function PrdPowerBarMixin:OnLoad()
+    self:SetupBorder()
+
     self.colorSet = false
 
-    SetupBorder(self)
-
     self:RegisterEvent("PLAYER_ENTERING_WORLD")
-    self:RegisterEvent("UNIT_POWER_UPDATE")
-    self:RegisterEvent("UNIT_MAXPOWER")
+    self:RegisterUnitEvent("UNIT_POWER_FREQUENT", "player")
+    self:RegisterUnitEvent("UNIT_MAXPOWER", "player")
 end
 
 function PrdPowerBarMixin:OnEvent()
@@ -82,4 +95,156 @@ function PrdPowerBarMixin:OnEvent()
     self:SetMinMaxValues(0, UnitPowerMax("player"))
     self:SetValue(UnitPower("player"))
     self.text:SetText(UnitPower("player") .. "/" .. UnitPowerMax("player"))
+end
+
+
+PrdPetHealthBarMixin = {}
+
+function PrdPetHealthBarMixin:OnLoad()
+    self:SetupBorder()
+
+    local _, class = UnitClass("player")
+
+    if class ~= "HUNTER" and class ~= "WARLOCK" then
+        self:Hide()
+    else
+        self:RegisterEvent("PLAYER_ENTERING_WORLD")
+        self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", "pet")
+        self:RegisterUnitEvent("UNIT_MAXHEALTH", "pet")
+    end
+end
+
+function PrdPetHealthBarMixin:OnEvent()
+    self:SetMinMaxValues(0, UnitHealthMax("pet"))
+    self:SetValue(UnitHealth("pet"))
+    self.text:SetText(UnitHealth("pet") .. "/" .. UnitHealthMax("pet"))
+end
+
+
+PrdAuraButtonMixin = {}
+
+function PrdAuraButtonMixin:OnEnter()
+end
+
+function PrdAuraButtonMixin:OnLeave()
+end
+
+
+local AURA_MAX_DISPLAY = 10
+
+AuraContainerMixing = {}
+
+function AuraContainerMixing:OnLoad()
+    self.auraPool = CreateFramePool("FRAME", self, "PrdAuraButtonTemplate")
+
+    self:RegisterUnitEvent("UNIT_AURA", "player")
+
+    self:OnUnitAuraUpdate("player")
+end
+
+function AuraContainerMixing:OnEvent(event, ...)
+    self:OnUnitAuraUpdate(...)
+end
+
+function AuraContainerMixing:OnUnitAuraUpdate(unit, unitAuraUpdateInfo)
+    local aurasChanged = false
+    if unitAuraUpdateInfo == nil or unitAuraUpdateInfo.isFullUpdate or self.auras == nil then
+        self:ParseAllAuras(unit)
+        aurasChanged = true
+    else
+        if unitAuraUpdateInfo.addedAuras ~= nil then
+            for _, aura in ipairs(unitAuraUpdateInfo.addedAuras) do
+                if self:ShouldShow(aura) and not C_UnitAuras.IsAuraFilteredOutByInstanceID(unit, aura.auraInstanceID, self:GetFilter()) then
+                    self.auras[aura.auraInstanceID] = aura
+                    aurasChanged = true
+                end
+            end
+        end
+        if unitAuraUpdateInfo.updatedAuraInstanceIDs ~= nil then
+            for _, auraInstanceID in ipairs(unitAuraUpdateInfo.updatedAuraInstanceIDs) do
+                if self.auras[auraInstanceID] ~= nil then
+                    local newAura = C_UnitAuras.GetAuraDataByAuraInstanceID(unit, auraInstanceID)
+                    self.auras[auraInstanceID] = newAura
+                    aurasChanged = true
+                end
+            end
+        end
+        if unitAuraUpdateInfo.removedAuraInstanceIDs ~= nil then
+            for _, auraInstanceID in ipairs(unitAuraUpdateInfo.removedAuraInstanceIDs) do
+                if self.auras[auraInstanceID] ~= nil then
+                    self.auras[auraInstanceID] = nil
+                    aurasChanged = true
+                end
+            end
+        end
+    end
+
+    if not aurasChanged then return end
+
+    self.auraPool:ReleaseAll()
+
+    local auraIndex = 1
+    self.auras:Iterate(function(auraInstanceID, aura)
+        local auraButton = self.auraPool:Acquire()
+        auraButton.auraInstanceID = auraInstanceID
+        auraButton.isauraButton = aura.isHelpful
+        auraButton.layoutIndex = auraIndex
+        auraButton.spellID = aura.spellId
+
+        auraButton.Icon:SetTexture(aura.icon)
+        if (aura.applications > 1) then
+            auraButton.CountFrame.Count:SetText(aura.applications)
+            auraButton.CountFrame.Count:Show()
+        else
+            auraButton.CountFrame.Count:Hide()
+        end
+        CooldownFrame_Set(auraButton.Cooldown, aura.expirationTime - aura.duration, aura.duration, aura.duration > 0, true)
+
+        auraButton:Show()
+
+        auraIndex = auraIndex + 1
+        return auraIndex >= AURA_MAX_DISPLAY
+    end)
+
+    self:Layout()
+end
+
+function AuraContainerMixing:ParseAllAuras(unit)
+    if self.auras == nil then
+        self.auras = TableUtil.CreatePriorityTable(AuraUtil.DefaultAuraCompare, TableUtil.Constants.AssociativePriorityTable)
+    else
+        self.auras:Clear()
+    end
+
+    local function HandleAura(aura)
+        if self:ShouldShow(aura) then
+            self.auras[aura.auraInstanceID] = aura
+        end
+
+        return false
+    end
+
+    AuraUtil.ForEachAura(unit, self:GetFilter(), nil, HandleAura, true)
+end
+
+
+BuffContainerMixin = CreateFromMixins(AuraContainerMixing)
+
+function BuffContainerMixin:GetFilter()
+    return "HELPFUL"
+end
+
+function BuffContainerMixin:ShouldShow()
+    return true
+end
+
+
+DebuffContainerMixin = CreateFromMixins(AuraContainerMixing)
+
+function DebuffContainerMixin:GetFilter()
+    return "HARMFUL"
+end
+
+function DebuffContainerMixin:ShouldShow()
+    return true
 end
